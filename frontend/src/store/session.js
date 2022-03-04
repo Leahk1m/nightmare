d18689bc16549a7664030f0b6748ebd26dd683e4
Leah Kim <lk.leahkim@gmail.com>
2022-03-03 17:16:57 -0800

    created signup page

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,6 +16,7 @@ const removeUser = () => {
   };
 };
 
+
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
@@ -54,5 +55,22 @@ export const restoreUser = () => async dispatch => {
     dispatch(setUser(data.user));
     return response;
   };
+
+
+export const signup = (user) => async dispatch => {
+    const { username, email, password } = user;
+    const response = await csrfFetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({
+            username,
+            email,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
 
 export default sessionReducer;
