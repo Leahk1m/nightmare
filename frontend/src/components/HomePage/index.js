f12c309fff1711e3018c0e2222662f58d5da0594
Leah Kim <lk.leahkim@gmail.com>
2022-03-04 21:22:58 -0800

    worked on splashpage

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -4,13 +4,15 @@ const HomePage = () => {
         <div className="splash-page-container">
             <div className="black-box-bg">
                 <div className="welcome-title-home">
-                    <img className="scaryhouse-pic" src="https://news.airbnb.com/wp-content/uploads/sites/4/2019/10/unique_press_1920x1280_A.jpg" alt="home-scary-house"></img>
-                    <h1 id="spooky-stays-title">
-                        Spooky stays you can visit all year-round
-                    </h1>
-                    <h2 id="haunted-home-title">
-                        Want to stay in a haunted home? Now you can. Scroll to explore utterly chilling stays.
-                    </h2>
+                    <img className="scaryhouse-pic" src="https://news.airbnb.com/wp-content/uploads/sites/4/2019/10/unique_press_1920x1280_A.jpg" alt="home-scary-house"/>
+                    <div className="banner-text">
+                        <h1 id="spooky-stays-title">
+                            Spooky stays you can visit all year-round
+                        </h1>
+                        <h2 id="haunted-home-title">
+                            Want to stay in a haunted home? Now you can. Scroll to explore utterly chilling stays.
+                        </h2>
+                    </div>
                 </div>
             </div>
 
