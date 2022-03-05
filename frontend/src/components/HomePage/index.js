3473ffdaba09add431d6e8964661fb77d413d7e9
Leah Kim <lk.leahkim@gmail.com>
2022-03-04 17:23:39 -0800

    added logo to navbar

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,7 +1,14 @@
-
+import './HomePage.css';
+import { useEffect, useState } from 'react';
 const HomePage = () => {
     return(
-        <div className="">
+        <div className="black-box-bg">
+            whee
+            <div className="welcome-blackbox-bg">
+                <img className="scaryhouse-pic" src="https://wallpapers.com/images/high/haunted-mansion-with-dead-tree-34573cw9etb752qi.jpg" alt="home-scary-house"></img>
+
+
+            </div>
 
         </div>
     );
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import logo from '../images/nightmarebnb.png';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
@@ -26,8 +27,8 @@ function Navigation({ isLoaded }){
   return (
     <div className="home-signup-login-nav">
         <div className="logo-home">
+            <img className="bat-logo" src={logo}/>
             <NavLink className="nav-link-path" id="nightmarebnb-nav-link" exact to="/">nightmarebnb</NavLink>
-
         </div>
 
         {isLoaded && sessionLinks}
