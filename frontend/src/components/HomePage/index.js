06f8322f10df94b7416f4a839f39dd687923fc84
Leah Kim <lk.leahkim@gmail.com>
2022-03-04 20:55:14 -0800

    added photo to splashpage

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,12 +1,23 @@
 import './HomePage.css';
-import { useEffect, useState } from 'react';
 const HomePage = () => {
     return(
-        <div className="black-box-bg">
-            whee
-            <div className="welcome-blackbox-bg">
-                <img className="scaryhouse-pic" src="https://wallpapers.com/images/high/haunted-mansion-with-dead-tree-34573cw9etb752qi.jpg" alt="home-scary-house"></img>
+        <div className="splash-page-container">
+            <div className="black-box-bg">
+                <div className="welcome-title-home">
+                    <img className="scaryhouse-pic" src="https://news.airbnb.com/wp-content/uploads/sites/4/2019/10/unique_press_1920x1280_A.jpg" alt="home-scary-house"></img>
+                    <h1 id="spooky-stays-title">
+                        Spooky stays you can visit all year-round
+                    </h1>
+                    <h2 id="haunted-home-title">
+                        Want to stay in a haunted home? Now you can. Scroll to explore utterly chilling stays.
+                    </h2>
+                </div>
+            </div>
 
+
+
+            <div className="white-box-bg">
+                <h2 className="haunting-homes-title">Haunting homes for resting your bones</h2>
 
             </div>
 
