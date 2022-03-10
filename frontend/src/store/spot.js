3ee49fb03102c1b9f42dda97ae421da15f7c10de
Leah Kim <lk.leahkim@gmail.com>
2022-03-10 12:38:00 -0800

    created utils folder in frontend with validation errors and added them into spot thunk

--- frontend/src/store/spot.js
+++ frontend/src/store/spot.js
@@ -1,4 +1,5 @@
 import { csrfFetch } from './csrf';
+import { ValidationError } from '../utils/validationError';
 
 const LOAD_SPOTS = 'spot/LOADSPOTS';
 const ADD_SPOT = 'spot/ADDSPOT';
@@ -74,6 +75,26 @@ export const getSpots = () => async (dispatch) => {
         imageUrl
       }),
     });
+
+    if (!response.ok) {
+        let error;
+        if (response.status === 422) {
+          error = await response.json();
+          throw new ValidationError(error.errors, response.statusText);
+        } else {
+          let errorJSON;
+          error = await response.text();
+          try {
+            // Check if the error is JSON, i.e., from the Spot server. If so,
+            // don't throw error yet or it will be caught by the following catch
+            errorJSON = JSON.parse(error);
+          } catch {
+            // Case if server could not be reached
+            throw new Error(error);
+          }
+          throw new Error(`${errorJSON.title}: ${errorJSON.message}`);
+        }
+      }
     const data = await response.json();
     // console.log(data)
     dispatch(addSpot(data));
