c4e76ecf6989ae06e78cd793c5c2b3bb094063af
Leah Kim <lk.leahkim@gmail.com>
2022-03-10 21:47:37 -0800

    can add favs

--- backend/routes/api/favorites.js
+++ backend/routes/api/favorites.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-
+const { handleValidationErrors } = require('../../utils/validation');
 const { Favorite } = require('../../db/models');
 
 const router = express.Router();
@@ -9,6 +9,22 @@ router.get('/', asyncHandler(async(req, res) => {
     const favorites = await Favorite.findAll();
     return res.json(favorites);
 
+}));
+
+router.post('/', handleValidationErrors, asyncHandler(async(req, res) => {
+    const details = req.body;
+    console.log(req.body)
+    const favorite = await Favorite.create(details);
+    res.json(favorite)
+
+}));
+
+router.delete('/:id', handleValidationErrors, asyncHandler(async(req, res) => {
+    const fav = Favorite.findByPk(req.params.id);
+    if(!fav) throw new Error('Cannot find favorite spot');
+
+    await Favorite.destroy({ where: {id: fav.id}});
+    return res.json({ id: fav.id}) //sending the id of the fav to frontend bc it requires a favId to delete
 }));
 
 
