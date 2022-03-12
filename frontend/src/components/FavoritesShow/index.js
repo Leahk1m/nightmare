2c3220b557b2893bb86eb346caec3dd53fbf6264
Leah Kim <lk.leahkim@gmail.com>
2022-03-11 20:42:32 -0800

    styling for fav spots page

--- frontend/src/components/FavoritesShow/index.js
+++ frontend/src/components/FavoritesShow/index.js
@@ -8,12 +8,16 @@ import { useDispatch, useSelector } from "react-redux";
 function FavoritesShow({ favData }) {
     const dispatch = useDispatch();
     const userId = useSelector(state => state.session.user.id);
-    // const favs = useSelector(state => state.favorites.allFavorites);
     const spots = useSelector(state => state.spot.objSpots);
     favData.filter((fav) => fav.userId === userId);
 
     return (
         <div className="fav-show-container">
+            <div>
+                <h1 className="spots-i-would-haunt-title">Spots That I would Haunt as a Ghost </h1>
+                <img className="in-love-ghost-pic" src="https://c.tenor.com/FZAf7pZFFh0AAAAC/blushing-ghost.gif" alt="in-love-ghost"/>
+
+            </div>
             {(favData || []).map((favSpot) => (
                 <div className="favSpot-container" key={favSpot.id}>
                     <h3>{spots[favSpot.spotId].name}</h3>
--- frontend/src/components/MySpots/index.js
+++ frontend/src/components/MySpots/index.js
@@ -8,22 +8,24 @@ function MySpots({ spotsData }) {
     return(
         <div className="my-spots-page-container">
             <span className="my-spots-pg-title-container">
-                <h2 classname="my-spots-pg-title">{username}'s Frightening Locations <img className="my-spots-pg-flying-ghosts" src="https://media1.giphy.com/media/fnfONXQS7MmHi2AcsJ/giphy.gif?cid=6c09b952k8x251k9mstv0csjpdeaf4xg4zm7ekqr7v5qtgpr&rid=giphy.gif&ct=s" alt="my-spots-pic"/></h2>
+                <h2 className="my-spots-pg-title">{username}'s Frightening Locations </h2>
+                <img className="my-spots-pg-flying-ghosts" src="https://media1.giphy.com/media/fnfONXQS7MmHi2AcsJ/giphy.gif?cid=6c09b952k8x251k9mstv0csjpdeaf4xg4zm7ekqr7v5qtgpr&rid=giphy.gif&ct=s" alt="my-spots-pic"/>
             </span>
 
- 
-            {myOwnSpots.map((spot) => (
-                <div key={spot.id}>
-                    <h3>{spot.name}</h3>
-                    <Link to={`/spots/${spot.id}`}>
-                        <img className="my-spots-pg-img" src={spot.imageUrl}/>
-                    </Link>
+            <div className="all-of-my-spots">
+                {myOwnSpots.map((spot) => (
+                    <div key={spot.id} className="my-spot-show-container">
+                        <h3>{spot.name}</h3>
+                        <Link to={`/spots/${spot.id}`}>
+                            <img className="my-spots-pg-img" src={spot.imageUrl}/>
+                        </Link>
 
-                    <h4>Location: {spot.city} {spot.state} {spot.country}</h4>
-                    <h4>${spot.price} / night</h4>
+                        <h4>Location: {spot.city} {spot.state} {spot.country}</h4>
+                        <h4>${spot.price} / night</h4>
 
-                </div>
-            ))}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
