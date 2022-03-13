fd543081fadfe8b7ca1255accb8710380ad08f34
Leah Kim <lk.leahkim@gmail.com>
2022-03-12 19:53:09 -0800

    added seeders to spot

---
+++ backend/db/migrations/20220313020134-create-spot.js
@@ -0,0 +1,73 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Spots', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        type: Sequelize.INTEGER
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING(100)
+      },
+      city: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      state: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      country: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.DECIMAL
+      },
+      imageUrl: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      imageUrlTwo: {
+        type: Sequelize.STRING
+      },
+      imageUrlThree: {
+        type: Sequelize.STRING
+      },
+      imageUrlFour: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Spots');
+  }
+};
